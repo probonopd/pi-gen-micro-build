fadde1a257d682c56da57d677e9260d3a1249d12
probonopd <probonopd@users.noreply.github.com>
2025-05-26 19:11:22 +0200

    sudo dpkg --force-depends -i "$DEB_PKG"

--- build.sh
+++ build.sh
@@ -14,7 +14,8 @@ debuild -uc -us
 
 # Install the built package (adjust version/arch if needed)
 DEB_PKG=$(ls ../pi-gen-micro_*_arm64.deb | head -n 1)
-sudo dpkg -i "$DEB_PKG"
+# sudo dpkg -i "$DEB_PKG"
+sudo dpkg --force-depends -i "$DEB_PKG"
 
 # Create an image using pi-gen-micro (example: fastboot configuration)
 WORKDIR=$(mktemp -d)
