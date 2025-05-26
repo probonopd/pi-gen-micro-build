3f34d69f885492551a4440d1e9388f61739adc23
probonopd <probonopd@users.noreply.github.com>
2025-05-26 18:53:38 +0200

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# Build image using https://github.com/raspberrypi/pi-gen-micro
+# NOTE: This works only on arm64 hosts
+
+set -e
+
+# Clone the pi-gen-micro repository
+git clone https://github.com/raspberrypi/pi-gen-micro.git
+cd pi-gen-micro
+
+# Build the package
+debuild -uc -us
+
+# Install the built package (adjust version/arch if needed)
+DEB_PKG=$(ls ../pi-gen-micro_*_arm64.deb | head -n 1)
+sudo dpkg -i "$DEB_PKG"
+
+# Create an image using pi-gen-micro (example: fastboot configuration)
+WORKDIR=$(mktemp -d)
+pushd "$WORKDIR"
+sudo pi-gen-micro fastboot
+popd
+
+echo "Image build complete! Check the $WORKDIR directory for output."
